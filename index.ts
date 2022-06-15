1471b5ac51b2fa937e56cbb03ab86ce0d4ca5358
Rafael Vargas <fael.vargas@hotmail.com>
2022-06-15 16:41:37 -0300

    Refatoração Index

--- index.ts
+++ index.ts
@@ -1,57 +1,12 @@
 import "reflect-metadata"
 import express from "express"
-import morgan from "morgan"
-import { DataSource } from "typeorm"
-
-const AppDataSource = new DataSource({
-  type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "abcbanana",
-  database: "netflix"
-})
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data source connected")
-  })
-  .catch((e) => {
-    console.log(e)
-  })
+import startRoutes from "./src/routers"
 
 const app: express.Application = express()
 
-const PORT = 3000
+const PORT = 8080
 
-const jsonParserMiddleware = express.json()
-
-app.use(jsonParserMiddleware)
-app.use(morgan('tiny'))
-
-app.get("/ping", (request, response) => {
-  response.send("pong")
-})
-
-app.post("/ping/:id", (request, response) => {
-  response.send("pong")
-})
-
-// Atualizaçao de dados
-// Entidade parcialmente
-app.patch("/ping/:id", (request, response) => {
-  response.send("pong")
-})
-
-// Atualizaçao de dados
-// Entidade inteira
-app.put("/ping/:id", (request, response) => {
-  response.send("pong")
-})
-
-app.delete("/ping/:id", (request, response) => {
-  response.send("pong")
-})
+startRoutes(app)
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta: ${PORT}`)
