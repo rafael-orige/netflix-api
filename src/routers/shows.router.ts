97d5159c56553197e53153486ec59058849ccca5
Rafael Vargas <fael.vargas@hotmail.com>
2022-06-16 18:09:34 -0300

    Rotas para listar um e deletar

--- src/routers/shows.router.ts
+++ src/routers/shows.router.ts
@@ -6,6 +6,11 @@ import createShow from "../schemas/create-show.schema"
 const ShowsRouter = Router()
 
 ShowsRouter.get("/show", ShowController.list)
+
+ShowsRouter.get("/show/:id", ShowController.listOne)
+
 ShowsRouter.post("/show", validationMiddleware(createShow), ShowController.create)
+
+ShowsRouter.delete("/show/:id", ShowController.delete)
 
 export default ShowsRouter
