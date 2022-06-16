669ec2bbbbd31fe327b56b01e51099d32d8d6a09
Rafael Vargas <fael.vargas@hotmail.com>
2022-06-16 18:08:44 -0300

    Crianção do controller para listar um e deletar

--- src/controllers/show.controller.ts
+++ src/controllers/show.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import { ShowService } from "../services"
-import HTTP_STATUS from "../constants/http-status.constants"
+import HTTP_STATUS from "../enums/http-status.enums"
+import errorHandler from "../middlewares/error-handler.middleware"
+
 const showService = new ShowService()
 
 class ShowController {
@@ -10,6 +12,17 @@ class ShowController {
     res.send(shows)
   }
 
+  public static async listOne(req: Request, res: Response) {
+    try {
+      const { params: { id } } = req
+      const show = await showService.listOne(+id)
+
+      res.json(show)
+    } catch (error) {
+      errorHandler(error, res)
+    }
+  }
+
   public static async create(req: Request, res: Response) {
     try {
       const show = req.body
@@ -17,8 +30,21 @@ class ShowController {
       const result = await showService.create(show)
 
       res.status(HTTP_STATUS.CREATED).send(result)
-    } catch (error: any) {
-      res.status(400).json({ error: error.message })
+    } catch (error) {
+      res.status(400)
+    }
+  }
+
+  public static async delete(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+
+      const result = await showService.delete(+id)
+
+      res.status(200).json(result)
+    } catch (error) {
+      console.log(error)
+      errorHandler(error, res)
     }
   }
 }
