2d58461745be8669491ae9867bdf7e36e8567b7f
Rafael Vargas <fael.vargas@hotmail.com>
2022-06-17 17:48:33 -0300

    Funções de exceções customizadas

---
+++ src/exceptions/index.ts
@@ -0,0 +1,5 @@
+export { default as BadRequestException } from "./bad-request.exception"
+export { default as NotFoundException } from "./not-found.exception"
+export { default as UnauthorizedException } from "./unauthorized.exception"
+export { default as HttpException } from "./http.exception"
+export { default as ValidationException } from "./validation.exception"
